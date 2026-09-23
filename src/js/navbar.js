5a94b2c504e64b4f2251a66c5ae554f8cb83f6e3
evamaran <evaand06353@stud.noroff.no>
2026-09-23 10:37:49 +0200

    Updated path in navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,12 +1,12 @@
 export async function initNavbar() {
-  const nav = document.querySelector(".site-nav");
-  if (!nav) return;
+	const nav = document.querySelector(".site-nav");
+	if (!nav) return;
 
-  try {
-    const response = await fetch("navbar.html");
-    const html = await response.text();
-    nav.innerHTML = html;
-  } catch (error) {
-    console.error("Could not load navbar:", error);
-  }
+	try {
+		const response = await fetch("/navbar.html");
+		const html = await response.text();
+		nav.innerHTML = html;
+	} catch (error) {
+		console.error("Could not load navbar:", error);
+	}
 }
